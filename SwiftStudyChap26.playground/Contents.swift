b5ad814c7efcf32895e05e7458a26fad668ade32
breaktimeSeok <breaktime_kr@naver.com>
2021-07-24 21:01:48 +0900

    2주차 스터디

---
+++ SwiftStudyChap26.playground/Contents.swift
@@ -0,0 +1,10 @@
+let arrayOfOptionalInts: [Int?] = [nil, 2, 3, nil, 5]
+
+for case let number? in arrayOfOptionalInts where number > 2 {
+    print("Found a \(number)")
+}
+
+/*
+ Found a 3
+ Found a 5
+ */
